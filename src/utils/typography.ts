453a4a15c762a1d5114e717b68b170fa3a070eaa
talohana <tal.ohana.x@gmail.com>
2020-10-03 00:21:06 +0300

    style(typography): changed body font-family to sans-serif

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,6 +1,9 @@
 import Typography from 'typography';
 import theme from 'typography-theme-irving';
 
+// TODO: Find a way to override this with overrideThemeStyles
+theme.bodyFontFamily = ['Open Sans', 'sans-serif'];
+
 const typography = new Typography(theme);
 
 export const { scale, rhythm, options } = typography;
