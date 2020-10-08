9308afdc1823b0e836af4673b5565cd680258c3a
talohana <tal.ohana.x@gmail.com>
2020-10-08 13:17:07 +0300

    fix(seo): prefixed thumbnail with site url

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -14,20 +14,29 @@ export const SEO: React.FC<Props> = ({
   return (
     <Helmet title={title}>
       <meta name="description" content={description} />
-      <meta name="image" content={config.seo.thumbnail} />
+      <meta
+        name="image"
+        content={`${config.seo.url}/${config.seo.thumbnail}`}
+      />
 
       <meta property="og:url" content={config.seo.url} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={config.seo.thumbnail} />
+      <meta
+        property="og:image"
+        content={`${config.seo.url}/${config.seo.thumbnail}`}
+      />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={config.social.twitter} />
       <meta name="twitter:site" content={config.social.twitterUsername} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image:src" content={config.seo.thumbnail} />
+      <meta
+        name="twitter:image:src"
+        content={`${config.seo.url}/${config.seo.thumbnail}`}
+      />
 
       <html lang="en" dir="ltr" />
     </Helmet>
