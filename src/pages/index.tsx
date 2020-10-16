5bfdb70b6128ff088c22761b720ba6844eaa97a8
talohana <tal.ohana.x@gmail.com>
2020-10-16 16:38:12 +0300

    fix(seo): added seo to index and 404 pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Element } from 'react-scroll';
 import { Layout } from '../components/common/Layout';
 import { Hero } from '../components/landing/Hero';
+import { SEO } from '../components/SEO/SEO';
 
 const IndexPage: React.FC = () => {
   return (
     <Layout>
+      <SEO />
       <Element name="hero">
         <Hero />
       </Element>
