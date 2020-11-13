083ed6f1a8533837eeb8e8ba9a90a84c6c256b8e
talohana <tal.ohana.x@gmail.com>
2020-11-13 14:22:47 +0200

    chore(jest): add tsconfig paths support

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { compilerOptions } = require('./tsconfig.json');
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const paths = pathsToModuleNameMapper(compilerOptions.paths, {
+  prefix: '<rootDir>/',
+});
+
 module.exports = {
   transform: {
     '^.+\\.[jt]sx?$': '<rootDir>/tests/jest-preprocess.js',
@@ -5,6 +13,7 @@ module.exports = {
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/tests/file-mock.js`,
+    ...paths,
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   watchPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
