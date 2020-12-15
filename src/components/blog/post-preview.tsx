ffb08e91c1797d17bcc464c8aae4e5c7eb11b98b
Tal Ohana <tal.ohana.x@gmail.com>
2020-12-15 11:36:46 +0200

    fix(post-preview): remove categories

--- src/components/blog/post-preview.tsx
+++ src/components/blog/post-preview.tsx
@@ -4,14 +4,13 @@ import Image, { FluidObject } from 'gatsby-image';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { Chip } from '../common/chip';
 
 type Props = {
   post: MdxFields;
 };
 
 export const PostPreview: React.FC<Props> = ({ post }) => {
-  const { title, description, date, slug, banner, categories } = post;
+  const { title, description, date, slug, banner } = post;
 
   if (!slug) {
     return null;
@@ -23,20 +22,9 @@ export const PostPreview: React.FC<Props> = ({ post }) => {
         <PreviewImage fluid={banner.childImageSharp.fluid as FluidObject} />
       )}
       <PreviewInfo>
-        <div>
-          {title && <h4 tw="text-primary my-2 uppercase">{title}</h4>}
-          {date && <span>{date}</span>}
-          {description && <p>{description}</p>}
-        </div>
-        {categories?.length && (
-          <Categories>
-            {categories.map(category => (
-              <li key={category}>
-                <StyledChip>{category}</StyledChip>
-              </li>
-            ))}
-          </Categories>
-        )}
+        {title && <h4 tw="text-primary my-2 uppercase">{title}</h4>}
+        {date && <span>{date}</span>}
+        {description && <p>{description}</p>}
       </PreviewInfo>
     </Wrapper>
   );
@@ -48,9 +36,5 @@ const Wrapper = styled(Link)`
 `;
 
 const PreviewImage = tw(Image)`h-1/2 md:w-1/2`;
-
-const Categories = tw.ul`hidden lg:flex space-x-2`;
-
-const StyledChip = tw(Chip)`px-1.5 py-0.5`;
 
 const PreviewInfo = tw.div`flex-1 p-1 md:p-4`;
