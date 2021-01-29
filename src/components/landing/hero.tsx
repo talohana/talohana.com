3df35971765f2879d6a253c7ae5d39f9fb8dba2d
Tal Ohana <tal.ohana.x@gmail.com>
2021-01-29 13:36:27 +0200

    feat(hero): change to svg pattern

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,13 +1,13 @@
-import { File } from '@types';
-import { graphql, useStaticQuery } from 'gatsby';
-import { FluidObject } from 'gatsby-image';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { BackgroundSection } from '../common/background-section';
 import { Container } from '../common/container';
 
-const Wrapper = styled(BackgroundSection)`
+const Wrapper = styled.section`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url(/images/hero-pattern.svg);
+  background-position: center;
+  background-repeat: repeat;
   height: 50vh;
 `;
 
@@ -23,28 +23,9 @@ const StyledContainer = styled(Container)`
   }
 `;
 
-const query = graphql`
-  query {
-    heroImage: file(relativePath: { eq: "hero.jpg" }) {
-      childImageSharp {
-        fluid(maxWidth: 1920) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
-`;
-
 export const Hero: React.FC = () => {
-  const { heroImage } = useStaticQuery<{ heroImage: File }>(query);
-
-  const backgroundImageStack = [
-    'linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75))',
-    heroImage.childImageSharp?.fluid as FluidObject,
-  ];
-
   return (
-    <Wrapper fluid={backgroundImageStack}>
+    <Wrapper>
       <StyledContainer>
         <h1>Hi There!</h1>
         <h2>I&apos;m Tal Ohana, a Software Engineer</h2>
