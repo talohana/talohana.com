2d7d5b7652cc75a6f9c8c31583052a8fd0e8f09d
Tal Ohana <tal.ohana.x@gmail.com>
2022-01-07 14:49:03 +0200

    feat(seo): update default SEO

--- utils/next-seo.config.ts
+++ utils/next-seo.config.ts
@@ -2,4 +2,13 @@ import { DefaultSeoProps } from 'next-seo';
 
 export const SEO: DefaultSeoProps = {
   titleTemplate: 'Tal Ohana | %s',
+  description: 'Tal Ohana - Software Engineer',
+  openGraph: {
+    site_name: 'Tal Ohana',
+    images: [{ url: 'https://talohana.com/assets/banner.png' }],
+  },
+  twitter: {
+    cardType: 'summary_large_image',
+    site: '@talohanax',
+  },
 };
