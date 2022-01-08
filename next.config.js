780e063f3942150532fba77ce5efd3c836328b20
Tal Ohana <tal.ohana.x@gmail.com>
2022-01-08 10:49:51 +0200

    feat(pwa): add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withPlugins = require('next-compose-plugins');
 const { withContentlayer } = require('next-contentlayer');
+const withPWA = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
 const nextConfiguration = {
   reactStrictMode: true,
+  swcMinify: true,
 };
 
-module.exports = withPlugins([withContentlayer()], nextConfiguration);
+module.exports = withPlugins(
+  [withContentlayer(), withPWA()],
+  nextConfiguration
+);
