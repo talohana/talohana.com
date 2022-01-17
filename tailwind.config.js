cff14c891a341dfdf2532a2606df2466a8b52314
Tal Ohana <tal.ohana.x@gmail.com>
2022-01-17 14:32:56 +0200

    feat: change font from Poppins to Inter var

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ module.exports = {
         },
       },
       fontFamily: {
-        sans: ['Poppins', ...fontFamily.sans],
+        sans: ['Inter var', ...fontFamily.sans],
       },
       typography: theme => ({
         DEFAULT: {
