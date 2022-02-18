b85bc57e6e6a54ec9e8e2bd6a085d98f6a8feaff
Tal Ohana <tal.ohana.x@gmail.com>
2022-02-18 10:38:09 +0200

    feat: add company (#418)

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,7 @@ export const links = {
   github: 'https://github.com/talohana',
   linkedin: 'https://www.linkedin.com/in/talohana/',
   twitter: 'https://twitter.com/talohanax',
+  company: 'https://about.google.com',
 };
 
 export const siteUrl = 'https://talohana.io';
