5aa835307619fa94a10d0c9fa71563a03bfb75e1
Tal Ohana <tal.ohana.x@gmail.com>
2024-05-03 10:18:41 +0300

    chore: use NextJS 14 recommended way to use include Google analytics (#571)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,23 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-              page_path: window.location.pathname,
-            });
-          `,
-            }}
-          />
-          {/* PWA */}
+            {/* PWA */}
           <link rel="manifest" href="/manifest.json" />
           <meta content="#ffffff" name="theme-color" />
           <link
